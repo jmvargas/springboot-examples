8c1a4afad02f7dae133c4bed947a483228f4e1fb
Jesús Manuel Vargas <jesusmanuel.vargas@ext.privalia.com>
2018-02-21 15:24:34 +0100

    Fix error with security

--- src/main/java/com/privalia/config/SpringSecConfig.java
+++ src/main/java/com/privalia/config/SpringSecConfig.java
@@ -2,9 +2,11 @@ package com.privalia.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SpringSecConfig {
+public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+    @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/","/swagger-resources").permitAll();
         httpSecurity.csrf().disable();
